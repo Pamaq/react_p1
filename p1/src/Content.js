4c590794525efabfb81b780a4f9f403781638bba
Paulina <paulinakuta00@gmail.com>
2023-07-22 16:15:41 +0200

    Chapter 4 course content

--- p1/src/Content.js
+++ p1/src/Content.js
@@ -6,9 +6,17 @@ const Conent = () => {
     const int = Math.floor(Math.random() * 3);
     return names[int];
   };
+  const handleClick = () => {
+    console.log("Click!");
+  };
+  const handleClick2 = (name) => {
+    console.log(`${name} was clicked`);
+  };
   return (
     <main>
       <p>Hello {handleNameChange()}!</p>
+      <button onClick={handleClick}>Click me!</button>
+      <button onClick={() => handleClick2("Dave")}>Click for name!</button>
     </main>
   );
 };
